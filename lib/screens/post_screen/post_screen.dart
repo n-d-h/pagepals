81da87d41030b6a99d442b060fbe7cd4f11db49b
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2024-03-12 22:46:28 +0700

    add refresh to post

--- lib/screens/post_screen/post_screen.dart
+++ lib/screens/post_screen/post_screen.dart
@@ -1,3 +1,4 @@
+import 'package:custom_refresh_indicator/custom_refresh_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:page_transition/page_transition.dart';
@@ -18,149 +19,162 @@ class _PostScreenState extends State<PostScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: CustomScrollView(
-          controller: _scrollController,
-          slivers: [
-            SliverAppBar(
-              backgroundColor: Colors.white,
-              title: const Text(
-                'PagePals and Friends',
-                style: TextStyle(
-                  color: Colors.greenAccent,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: -1.2,
-                ),
-              ),
-              automaticallyImplyLeading: false,
-              centerTitle: false,
-              floating: true,
-              stretch: true,
-              pinned: true,
-              surfaceTintColor: Colors.white,
-              actions: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 10),
-                  child: IconButton(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        PageTransition(
-                          child: const MessageScreen(),
-                          type: PageTransitionType.bottomToTop,
-                          duration: const Duration(milliseconds: 500),
-                        ),
-                      );
-                    },
-                    splashRadius: 24,
-                    icon: const Icon(
-                      UniconsLine.comment_lines,
-                      color: Colors.black,
-                    ),
+    return CustomMaterialIndicator(
+      onRefresh: () {
+        return Future<void>.delayed(const Duration(seconds: 1));
+      },
+      indicatorBuilder: (BuildContext context, IndicatorController controller) {
+        return const Icon(
+          UniconsLine.book_open,
+          color: Colors.blueAccent,
+          size: 35,
+          semanticLabel: 'Pull to refresh',
+        );
+      },
+      child: SafeArea(
+        child: Scaffold(
+          body: CustomScrollView(
+            controller: _scrollController,
+            slivers: [
+              SliverAppBar(
+                backgroundColor: Colors.white,
+                title: const Text(
+                  'PagePals and Friends',
+                  style: TextStyle(
+                    color: Colors.blueAccent,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: -1.2,
                   ),
                 ),
-              ],
-            ),
-            SliverList(
-              delegate: SliverChildListDelegate(
-                [
+                automaticallyImplyLeading: false,
+                centerTitle: false,
+                floating: true,
+                stretch: true,
+                pinned: true,
+                surfaceTintColor: Colors.white,
+                actions: [
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Row(
-                      children: [
-                        const CircleAvatar(
-                          radius: 24,
-                          backgroundImage:
-                              AssetImage('assets/image_reader.png'),
-                        ),
-                        const SizedBox(width: 20),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.7,
-                          child: InkWell(
-                            borderRadius: BorderRadius.circular(10),
-                            onTap: () {
-                              Navigator.of(context).push(
-                                PageTransition(
-                                  child: const PostStatusScreen(),
-                                  type: PageTransitionType.fade,
-                                  duration: const Duration(milliseconds: 300),
-                                ),
-                              );
-                            },
-                            child: Container(
-                              padding: const EdgeInsets.all(9),
-                              child: Text(
-                                AppLocalizations.of(context)!
-                                    .appWhatAreYouThinking,
-                                style: const TextStyle(
-                                  color: Colors.grey,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
+                    padding: const EdgeInsets.only(right: 10),
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          PageTransition(
+                            child: const MessageScreen(),
+                            type: PageTransitionType.bottomToTop,
+                            duration: const Duration(milliseconds: 500),
+                          ),
+                        );
+                      },
+                      splashRadius: 24,
+                      icon: const Icon(
+                        UniconsLine.comment_lines,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SliverList(
+                delegate: SliverChildListDelegate(
+                  [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Row(
+                        children: [
+                          const CircleAvatar(
+                            radius: 24,
+                            backgroundImage:
+                                AssetImage('assets/image_reader.png'),
+                          ),
+                          const SizedBox(width: 20),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.7,
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(10),
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  PageTransition(
+                                    child: const PostStatusScreen(),
+                                    type: PageTransitionType.fade,
+                                    duration: const Duration(milliseconds: 300),
+                                  ),
+                                );
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.all(9),
+                                child: Text(
+                                  AppLocalizations.of(context)!
+                                      .appWhatAreYouThinking,
+                                  style: const TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 12),
-                  SizedBox(
-                    width: double.infinity,
-                    child: Column(
-                      children: [
-                        PostItem(
-                          username: 'John Doe',
-                          timeAgo:
-                              '2 ${AppLocalizations.of(context)!.appHoursAgo}',
-                          postText:
-                              'This is a post text sdklfjhskdjhffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffsdklfjhgsdlgdgggggggggggdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgd',
-                          imageUrl:
-                              'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg',
-                        ),
-                        PostItem(
-                          username: 'John Doe',
-                          timeAgo:
-                              '2 ${AppLocalizations.of(context)!.appHoursAgo}',
-                          postText: 'This is a post text',
-                          imageUrl:
-                              'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg',
-                        ),
-                        PostItem(
-                          username: 'John Doe',
-                          timeAgo:
-                              '2 ${AppLocalizations.of(context)!.appHoursAgo}',
-                          postText: 'This is a post text',
-                          imageUrl:
-                              'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg',
-                        ),
-                        PostItem(
-                          username: 'John Doe',
-                          timeAgo:
-                              '2 ${AppLocalizations.of(context)!.appHoursAgo}',
-                          postText: 'This is a post text',
-                          imageUrl:
-                              'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg',
-                        ),
-                        PostItem(
-                          username: 'John Doe',
-                          timeAgo:
-                              '2 ${AppLocalizations.of(context)!.appHoursAgo}',
-                          postText: 'This is a post text',
-                          imageUrl:
-                              'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg',
-                        ),
-                        const SizedBox(
-                          height: 75,
-                        )
-                      ],
-                    ),
-                  )
-                ],
+                    const SizedBox(height: 12),
+                    SizedBox(
+                      width: double.infinity,
+                      child: Column(
+                        children: [
+                          PostItem(
+                            username: 'John Doe',
+                            timeAgo:
+                                '2 ${AppLocalizations.of(context)!.appHoursAgo}',
+                            postText:
+                                'This is a post text sdklfjhskdjhffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffsdklfjhgsdlgdgggggggggggdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgd',
+                            imageUrl:
+                                'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg',
+                          ),
+                          PostItem(
+                            username: 'John Doe',
+                            timeAgo:
+                                '2 ${AppLocalizations.of(context)!.appHoursAgo}',
+                            postText: 'This is a post text',
+                            imageUrl:
+                                'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg',
+                          ),
+                          PostItem(
+                            username: 'John Doe',
+                            timeAgo:
+                                '2 ${AppLocalizations.of(context)!.appHoursAgo}',
+                            postText: 'This is a post text',
+                            imageUrl:
+                                'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg',
+                          ),
+                          PostItem(
+                            username: 'John Doe',
+                            timeAgo:
+                                '2 ${AppLocalizations.of(context)!.appHoursAgo}',
+                            postText: 'This is a post text',
+                            imageUrl:
+                                'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg',
+                          ),
+                          PostItem(
+                            username: 'John Doe',
+                            timeAgo:
+                                '2 ${AppLocalizations.of(context)!.appHoursAgo}',
+                            postText: 'This is a post text',
+                            imageUrl:
+                                'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg',
+                          ),
+                          const SizedBox(
+                            height: 75,
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
