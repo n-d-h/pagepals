421774ddbb980272aa6a460c8e9158f4df948941
n-d-h <duchoang4195@gmail.com>
2024-03-17 16:38:00 +0700

    commit

--- lib/screens/home_screen/popular_readers_widgets/popular_reader_shimmer.dart
+++ lib/screens/home_screen/popular_readers_widgets/popular_reader_shimmer.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:pagepals/helpers/color_helper.dart';
 import 'package:shimmer/shimmer.dart';
 
 class PopularReaderShimmer extends StatelessWidget {
