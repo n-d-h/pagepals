d4fd4a1aee6139b8aa60df5f9100f631f4b13458
n-d-h <duchoang4195@gmail.com>
2024-04-12 20:13:22 +0700

    commit

--- lib/screens/screens_reader/reader_request/reader_request_step2.dart
+++ lib/screens/screens_reader/reader_request/reader_request_step2.dart
@@ -82,17 +82,6 @@ class _QuestionWidgetState extends State<QuestionWidget> {
 
   @override
   Widget build(BuildContext context) {
-    // final readerProvider = context.watch<ReaderRequestProvider>();
-    // final readerRequestModel = readerProvider.readerRequestModel;
-    // setState(() {
-    //   _controller.text = readerRequestModel.answers
-    //           ?.firstWhere(
-    //             (element) => element?.questionId == widget.question.id,
-    //             orElse: () => null,
-    //           )
-    //           ?.content ??
-    //       '';
-    // });
     return Container(
       padding: const EdgeInsets.fromLTRB(17, 0, 17, 17),
       margin: const EdgeInsets.only(bottom: 10),
